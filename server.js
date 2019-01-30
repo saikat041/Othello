954294ffdb705036703b107238ed25b9e2fefc88
Saikat Sarkar <saikat.dakshana15@gmail.com>
2019-01-31 04:20:02 +0530

    Chnages in port for heroku deployment

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 app = express()
 app.use(express.static(__dirname+'/public'))
-var port = 8080
+var port = process.env.PORT || 8080
 
 var server = app.listen(port, function(){
     var port = server.address().port
